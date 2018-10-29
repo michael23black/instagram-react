71995ccc1c9a942ff1dcc3ede391be55c362eef6
ShaggyYeti <master205@mail.ru>
2018-10-30 01:28:12 +0300

    - изображения перемещены в папку public - переработан механизм вывода списка изображений

--- src/containers/CPosts/CPosts.jsx
+++ src/containers/CPosts/CPosts.jsx
@@ -3,24 +3,6 @@ import styled from 'styled-components';
 
 import Post from './../../components/Post/Post.jsx';
 
-import Image1 from './p/1.png';
-import Image2 from './p/2.png';
-import Image3 from './p/3.png';
-import Image4 from './p/4.png';
-import Image5 from './p/5.png';
-import Image6 from './p/6.png';
-import Image7 from './p/7.png';
-import Image8 from './p/8.png';
-import Image9 from './p/9.png';
-import Image10 from './p/10.png';
-import Image11 from './p/11.png';
-import Image12 from './p/12.png';
-import Image13 from './p/13.png';
-import Image14 from './p/14.png';
-import Image15 from './p/15.png';
-import Image16 from './p/16.png';
-import Image17 from './p/17.png';
-
 const SCPosts = styled.div`
     display: flex;
     flex-flow: row wrap;
@@ -32,27 +14,35 @@ const SCPosts = styled.div`
 	}
 `
 
+const posts = [
+  {url: '/img/p/1.png', likes: '100k', comments: 945 },
+  {url: '/img/p/2.png', likes: '100k', comments: 450 },
+  {url: '/img/p/3.png', likes: '100k', comments: 143 },
+  {url: '/img/p/4.png', likes: '100k', comments: 65 },
+  {url: '/img/p/5.png', likes: '100k', comments: 655 },
+  {url: '/img/p/6.png', likes: '100k', comments: 99 },
+  {url: '/img/p/7.png', likes: '100k', comments: 656 },
+  {url: '/img/p/8.png', likes: '100k', comments: 213 },
+  {url: '/img/p/9.png', likes: '100k', comments: 145 },
+  {url: '/img/p/10.png', likes: '100k', comments: 83 },
+  {url: '/img/p/11.png', likes: '100k', comments: 445 },
+  {url: '/img/p/12.png', likes: '100k', comments: 843 },
+  {url: '/img/p/13.png', likes: '100k', comments: 156 },
+  {url: '/img/p/14.png', likes: '100k', comments: 444 },
+  {url: '/img/p/15.png', likes: '100k', comments: 378 },
+  {url: '/img/p/16.png', likes: '100k', comments: 144 },
+  {url: '/img/p/17.png', likes: '100k', comments: 967 },
+];
+
 export default class CPosts extends React.Component {
     render() {
         return(
             <SCPosts>
-               <Post image={Image1} likes='100k' comments='945'/>
-               <Post image={Image2} likes='100k' comments='450'/>
-               <Post image={Image3} likes='100k' comments='143'/>
-               <Post image={Image4} likes='100k' comments='65'/>
-               <Post image={Image5} likes='100k' comments='655'/> 
-               <Post image={Image6} likes='100k' comments='99'/>
-               <Post image={Image7} likes='100k' comments='656'/>
-               <Post image={Image8} likes='100k' comments='213'/>
-               <Post image={Image9} likes='100k' comments='145'/>
-               <Post image={Image10} likes='100k' comments='83'/>
-               <Post image={Image11} likes='100k' comments='445'/>
-               <Post image={Image12} likes='100k' comments='843'/>
-               <Post image={Image13} likes='100k' comments='156'/>
-               <Post image={Image14} likes='100k' comments='444'/>
-               <Post image={Image15} likes='100k' comments='378'/>
-               <Post image={Image16} likes='100k' comments='144'/>
-               <Post image={Image17} likes='100k' comments='967'/>
+              {
+                posts.map(post => 
+                <Post image={post.url} likes={post.likes} comments={post.comments}/>
+                )
+              }
             </SCPosts>
         )
     }
