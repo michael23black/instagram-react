c059090c611b4828a4665a98839067387440759b
ShaggyYeti <master205@mail.ru>
2018-10-30 01:13:52 +0300

    - переименование CMain jsx в js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
-import Main from './containers/CMain/CMain.jsx';
+import Main from './containers/CMain/CMain';
 
 
 import './index.css';
